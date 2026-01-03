a2beefd13ebb0d6db90cfea3c39ea1931f503dca
Hector <hveitia86@gmail.com>
2026-01-03 10:53:06 -0500

    Bump application version to 1.0.1.
    - **Android**: Update `versionCode` to 3 and `versionName` to 1.0.1 in `build.gradle.kts`.
    - **iOS**: Update `CURRENT_PROJECT_VERSION` to 6 and set `MARKETING_VERSION` to 1.0.1 in `project.pbxproj`.
    - **Pubspec**: Update project version to 1.0.1.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         }
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
     }
 
     signingConfigs {
